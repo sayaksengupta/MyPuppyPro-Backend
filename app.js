509b7d4baf9c0662392159d6b8de4e8514913a68
sayakFortMindz <124573594+sayakFortMindz@users.noreply.github.com>
2023-09-24 14:17:40 +0530

    s3

--- app.js
+++ app.js
@@ -13,11 +13,60 @@ require("./routes/startup/routes")(app);
 const port = process.env.PORT || 8080;
 require("./db/conn");
 const connection = require("./db/conn");
+const AWS = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 
+let s3 = new AWS.S3({
+  region: "us-east-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 connection();
 
+// s3.createBucket(
+//   {
+//     Bucket: "my-puppy-pro",
+//   },
+//   (error, success) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     console.log(success);
+//   }
+// );
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.BUCKET_NAME,
+    metadata: (req, file, cb) => {
+      cb(null, {
+        fieldName: file.fieldname,
+      });
+    },
+    key: (request, file, cb) => {
+      fileName = file.originalname.replace(/\s+/g, "%20");
+      cb(null, file.originalname);
+    },
+  }),
+}).single("file");
+
+app.post("/upload", function (request, response, next) {
+  upload(request, response, function (error) {
+    if (error) {
+      console.log(error);
+      return response.status(500).json({ message: error });
+    }
+    response.status(200).json({
+      message: "File uploaded successfully.",
+      success: true,
+      fileUrl: `${process.env.S3_ENDPOINT}${fileName}`,
+    });
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+  console.log(`Listening to port ${port}`);
 });
