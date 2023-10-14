8e01de4e3ce148f0e8441c073f0f5e3c527869c6
sayaksengupta <sayaksengupta74@gmail.com>
2023-10-15 01:27:33 +0530

    fix

--- models/users.js
+++ models/users.js
@@ -24,11 +24,6 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Invalid Email.");
-      }
-    },
   },
   password: {
     type: String,
