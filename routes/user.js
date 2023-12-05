d3e86fe63d1a7c93dde49a721e62621c171bbf31
sayakATPL <sayak.sengupta@atpl.live>
2023-12-05 15:43:32 +0530

    fix rating

--- routes/user.js
+++ routes/user.js
@@ -1248,7 +1248,7 @@ router.post("/add-dog-rating/:dogId", userAuth, async (req, res) => {
     const averageRating = totalRatings / dog.ratings.length;
 
     // Update the dog's average rating
-    dog.averageRating = averageRating;
+    dog.averageRating = averageRating? averageRating : 0;
 
     // Save the dog document with the updated rating and average rating
     await dog.save();
